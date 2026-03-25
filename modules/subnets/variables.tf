6ea03b83651459e16634154cc865a135cb1b47ea
krishna <krishnascm21@gmail.com>
2026-03-25 22:20:20 +0530

    added outputs.tf 1

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -2,11 +2,12 @@ variable "vpc_id" {
   description = "VPC ID"
   type        = string
 }
-variable "private_subnets" {
-  description = "private_subnets"
-  type        = string
-}
+
+
 variable "public_subnets" {
-  description = "public_subnets"
-  type        = string
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
 }
